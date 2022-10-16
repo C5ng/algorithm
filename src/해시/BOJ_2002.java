95b0709e92934b3eca0bed8e5099a621ee290b07
DESKTOP-JOJ5L1Q\82109 <c5ngdp@gmail.com>
2022-10-17 03:54:20 +0900

    Algorithm

--- src/해시/BOJ_2002.java
+++ src/해시/BOJ_2002.java
@@ -1,5 +1,7 @@
 package 해시;
 
+/* https://www.acmicpc.net/problem/2002 */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,20 +11,37 @@ public class BOJ_2002 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         HashMap<String, Integer> hm = new HashMap<>();
-        int count = 0;
+        int answer = 0;
 
         int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
             hm.put(br.readLine(), i);
         }
 
+        int[] out = new int[n];
         for (int i = 0; i < n; i++) {
-            if (hm.get(br.readLine()) > i) {
-                count++;
+            out[i] = hm.get(br.readLine());
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (out[i] > out[j]) {
+                    answer++;
+                    break;
+                }
             }
         }
 
-        System.out.print(count);
-        
+//        for (int i = 0; i < n; i++) {
+//            String str = br.readLine();
+//            if (hm.get(str) == count) {
+//                count++;
+//            } else {
+//                answer++;
+//            }
+//        }
+
+        System.out.print(answer);
+
     }
 }
