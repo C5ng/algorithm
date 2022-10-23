24dddba18edac9671fad999d29eaca5fda5d1b4a
DESKTOP-JOJ5L1Q\82109 <c5ngdp@gmail.com>
2022-10-23 23:29:23 +0900

    Algorithm

---
+++ src/알고리즘_분류별/문자열/BOJ_2675.java
@@ -0,0 +1,32 @@
+package 알고리즘_분류별.문자열;
+
+/* https://www.acmicpc.net/problem/2675 */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2675 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int tc = Integer.parseInt(br.readLine());
+        for (int i = 0; i < tc; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            String s = st.nextToken();
+
+            for (int j = 0; j < s.length(); j++) {
+                for (int k = 0; k < r; k++) {
+                    sb.append(s.charAt(j));
+                }
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
