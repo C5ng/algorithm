39823e748442ed7ebd840818492b56d77255001e
DESKTOP-JOJ5L1Q\82109 <c5ngdp@gmail.com>
2022-10-25 22:49:24 +0900

    Algorithm

---
+++ src/알고리즘_분류별/문자열/BOJ_11718.java
@@ -0,0 +1,19 @@
+package 알고리즘_분류별.문자열;
+
+/* https://www.acmicpc.net/problem/11718 */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = "";
+
+        while((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
+    }
+}
