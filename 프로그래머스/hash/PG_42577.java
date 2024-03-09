fc18a606e191401faaf6a87aa79a2e2c268d77ba
DESKTOP-JOJ5L1Q\82109 <c5ngsdp@gmail.com>
2024-03-09 18:15:50 +0900

    Algorithm

--- 프로그래머스/hash/PG_42577.java
+++ 프로그래머스/hash/PG_42577.java
@@ -1,6 +1,8 @@
 package hash;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PG_42577 {
     public static void main(String[] args) {
@@ -10,13 +12,18 @@ public class PG_42577 {
     public static boolean solution(String[] phone_book) {
         boolean answer = true;
 
-        Arrays.sort(phone_book);
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < phone_book.length; i++) {
+            for (int j = 0; j < phone_book[i].length() - 1; j++) {
+                map.put(phone_book[i].substring(0, j + 1), 1);
+            }
+        }
 
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            for (int j = i + 1; j < phone_book.length; j++) {
-                if (phone_book[j].startsWith(phone_book[i])) {
-                    answer = false;
-                }
+
+        for (int i = 0; i < phone_book.length; i++) {
+            if (map.containsKey(phone_book[i])) {
+                answer = false;
+                break;
             }
         }
 
