54953adc5c5425097252dbce402e64a1920362c9
DESKTOP-JOJ5L1Q\82109 <c5ngsdp@gmail.com>
2024-03-19 04:07:00 +0900

    Algorithm

---
+++ 백준/greedy/BOJ_11399.java
@@ -0,0 +1,33 @@
+package greedy;
+
+/* https://www.acmicpc.net/problem/11399 */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_11399 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int answer = 0;
+        int count = 0;
+        for (int i : arr) {
+            answer += i + count;
+            count += i;
+        }
+
+        System.out.print(answer);
+    }
+}
